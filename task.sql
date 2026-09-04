17c7fb0ceb04def49704c96414e7e2367849afe9
Olena Nosenko <nosenko.alena@gmail.com>
2026-09-04 16:03:08 +0300

    updated implementation for using database engines to MySQL

--- task.sql
+++ task.sql
@@ -31,7 +31,7 @@ CREATE TABLE Logs (
     Timestamp TIMESTAMP, 
     Message VARCHAR(100),
     PRIMARY KEY (ID)
-) ENGINE=Blackhole;
+) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
