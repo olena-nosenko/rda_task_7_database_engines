a04e0af20fff0c080c17f6d56b3a3968b87764cd
Olena Nosenko <nosenko.alena@gmail.com>
2026-09-04 15:58:53 +0300

    updated implementation for using database engines to MySQL

--- task.sql
+++ task.sql
@@ -23,7 +23,7 @@ CREATE TABLE ProductDescription (
     ProductID INT,
     CountryID INT,
     PRIMARY KEY (ID)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
